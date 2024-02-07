98632249176118a20fa97a6987a6b064a4f88cf9
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-07 16:30:29 +0100

    add js-conditions-and-booleans_statements and solve challenge

---
+++ js-conditions-and-booleans_statements/index.js
@@ -0,0 +1,46 @@
+console.clear();
+
+// Part 1: Password
+const SUPER_SECRET_PASSWORD = "h4x0r1337";
+
+const receivedPassword = "password1234";
+
+if (receivedPassword === SUPER_SECRET_PASSWORD) {
+  console.log("Welcome! You are logged in as Brunhilde1984.");
+} else {
+  console.log("Access denied!");
+}
+
+// Part 2: Even / Odd
+const number = 6;
+if (number % 2 === 0) {
+  console.log("even number");
+} else {
+  console.log("odd number");
+}
+
+// Part 3: Hotdogs
+const numberOfHotdogs = 42;
+if (numberOfHotdogs < 5) {
+  console.log("2 euro per hotdog");
+} else if (numberOfHotdogs > 5 && numberOfHotdogs < 100) {
+  console.log("1.50 euro per hotdog");
+} else if (numberOfHotdogs > 100 && numberOfHotdogs < 1000000) {
+  console.log("1 euro per hotdog");
+} else if (numberOfHotdogs > 1000000) {
+  console.log("0.10 euro per hotdog");
+}
+
+// Part 4: Daytime
+const currentHour = 12;
+
+const statement = currentHour < 17 ? "Still need to learn..." : "Partytime!!!";
+
+console.log(statement);
+
+// Part 5: Greeting
+const name = "Archibald";
+
+const greeting = "Hello " + (name == "Archibald" ? name : "Coach") + "!";
+
+console.log(greeting);
