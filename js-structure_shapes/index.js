8589457804b03a1cc1633aba7aeed884e658c310
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-16 15:42:14 +0100

    solve task2

--- js-structure_shapes/index.js
+++ js-structure_shapes/index.js
@@ -1,24 +1,15 @@
-import { getRandomColor } from "./utils/randomColor.js";
+import { Circle } from "./components/Circle/Circle.js";
+import { Square } from "./components/Square/Square.js";
+import { Pentagon } from "./components/Pentagon/Pentagon.js";
 console.clear();
 
 const root = document.getElementById("root");
 
-const circle = document.createElement("div");
-circle.classList.add("circle");
-circle.addEventListener("click", () => {
-  circle.style.backgroundColor = getRandomColor(circle);
-});
+const circleElement = Circle();
+root.append(circleElement);
 
-const square = document.createElement("div");
-square.classList.add("square");
-square.addEventListener("click", () => {
-  square.style.backgroundColor = getRandomColor(square);
-});
+const squareElement = Square();
+root.append(squareElement);
 
-const pentagon = document.createElement("div");
-pentagon.classList.add("pentagon");
-pentagon.addEventListener("click", () => {
-  pentagon.style.backgroundColor = getRandomColor(pentagon);
-});
-
-root.append(circle, square, pentagon);
+const pentagonElement = Pentagon();
+root.append(pentagonElement);
