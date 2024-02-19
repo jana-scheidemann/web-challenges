4b46f4263ac02184c6759cc5c8d2e1e0bb914a8e
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-19 16:31:49 +0100

    add js-fetch_star-wars-console

---
+++ js-fetch_star-wars-console/js/index.js
@@ -0,0 +1,22 @@
+console.clear();
+
+const url = "https://swapi.dev/api/people";
+
+async function fetchData() {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.results;
+}
+fetchData();
+
+// - log the entire data object
+console.log(await fetchData());
+
+// - log different values of the data object
+const results = await fetchData();
+const nameOfIndex0 = results[0].name;
+console.log(nameOfIndex0);
+
+// - Bonus: Can you log the eye color of R2-D2?
+const eyeColorOfR2D2 = results[2].eye_color;
+console.log(eyeColorOfR2D2);
