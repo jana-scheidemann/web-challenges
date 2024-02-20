c952a4ed35484b4288ff2d5e59ec6b1737b83b62
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-20 11:16:57 +0100

    add js-error-handling_calculator, solve challenge

---
+++ js-error-handling_calculator/js/index.js
@@ -0,0 +1,31 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+const errorOutput = document.querySelector(".error");
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    try {
+      if (b === 0) {
+        throw new Error("Cannot divide by zero!");
+      }
+      return a / b;
+    } catch (error) {
+      console.log(error);
+      errorOutput.innerText =
+        "Be smart and pass a number rather than 0 as divisor, thank you!";
+    }
+  },
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumber = Number(event.target.firstNumber.value);
+  const secondNumber = Number(event.target.secondNumber.value);
+  const operation = event.target.operation.value;
+  output.innerText = operations[operation](firstNumber, secondNumber);
+});
