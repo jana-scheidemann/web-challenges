47ba9d8500fa3734cfb63d7db6b0e52fff3e55e1
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-26 16:02:26 +0100

    add folder, solve challenge

---
+++ react-state_counter/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <div className="button-container">
+        <button
+          type="button"
+          onClick={() => {
+            setCount(count - 1);
+            console.log("🤔");
+          }}
+        >
+          -
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            setCount(count + 1);
+            console.log("🤔");
+          }}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
