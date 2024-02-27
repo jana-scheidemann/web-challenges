dd9533c84f62b3eb7be71adc03f6ae903cafabe2
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-27 16:57:51 +0100

    add folder, not solve challenge

---
+++ react-state-2_table-reservation/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Counter from "./components/Counter";
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [people, setPeople] = useState(0);
+
+  function handleSubtract() {
+    setPeople(people - 1);
+  }
+
+  function handleAdd() {
+    setPeople(people + 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>Place a Table Reservation</h1>
+      <Counter onAdd={handleAdd} onSubtract={handleSubtract} />
+      <p>You are going to reserve a table for {setPeople} people.</p>
+    </div>
+  );
+}
