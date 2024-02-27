83737c45db65df0f171668d36d25c9e769f61300
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-27 20:18:10 +0100

    solve bonus

--- react-with-arrays_fruits/src/App.js
+++ react-with-arrays_fruits/src/App.js
@@ -2,26 +2,20 @@ import Card from "./components/Card";
 
 export default function App() {
   const fruits = [
-    { id: 26, name: "mango", color: "yellowred" },
-    { id: 5, name: "apple", color: "yellowred" },
-    { id: 77, name: "kiwi", color: "green" },
-    { id: 87, name: "pineapple", color: "yellow" },
-    { id: 1, name: "orange", color: "orange" },
+    { id: 26, name: "🥭 mango", color: "lightgreen" },
+    { id: 5, name: "🍎 apple", color: "red" },
+    { id: 77, name: "🥝 kiwi", color: "green" },
+    { id: 87, name: "🍍 pineapple", color: "yellow" },
+    { id: 1, name: "🍊 orange", color: "orange" },
     { id: 90, name: "passionfruit", color: "purple" },
-    { id: 9, name: "banana", color: "yellow" },
+    { id: 9, name: "🍌 banana", color: "yellow" },
   ];
 
   return (
     <div className="app">
-      <article>
-        {fruits.map(({ name, id }) => {
-          return (
-            <p key={id}>
-              <Card name={name} />
-            </p>
-          );
-        })}
-      </article>
+      {fruits.map(({ name, id, color }) => {
+        return <Card key={id} name={name} color={color} />;
+      })}
     </div>
   );
 }
