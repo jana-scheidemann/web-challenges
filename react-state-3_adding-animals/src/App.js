a69ff24b40792d6cf48d9d8890fd6486b2817207
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-28 13:05:30 +0100

    add folder, solve challenge

---
+++ react-state-3_adding-animals/src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./styles.css";
+import Form from "./components/Form/index.js";
+import List from "./components/List/index.js";
+import { uid } from "uid";
+
+const initialAnimals = [
+  {
+    id: "mTBErig",
+    name: "Octopus",
+    emoji: "🐙",
+  },
+  {
+    id: "iMsbXQ1",
+    name: "Crocodile",
+    emoji: "🐊",
+  },
+  {
+    id: "Qjf_K7V",
+    name: "Beaver",
+    emoji: "🦫",
+  },
+];
+
+export default function App() {
+  const [animals, setAnimals] = useState(initialAnimals);
+
+  function handleAddAnimal(newAnimal) {
+    // console.log(newAnimal);
+    setAnimals([
+      ...animals,
+      { name: newAnimal.name, emoji: newAnimal.emoji, id: uid() },
+    ]);
+    //  ODER: setAnimals([
+    //   ...animals,
+    //   { ...newAnimal, id: uid() },
+    // ]);
+  }
+
+  return (
+    <main className="app">
+      <Form onAddAnimal={handleAddAnimal} />
+      <List animals={animals} />
+    </main>
+  );
+}
