35be95c1c530f1a1e9d93c74a53764675c39a125
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-02-29 15:05:51 +0100

    solve Task 3

---
+++ react-with-arrays_users/src/components/Card.js
@@ -0,0 +1,23 @@
+import "./Card.css";
+import Tag from "./Tag";
+import Button from "./Button";
+// import { USERS } from "../db";
+
+function Card({ user }) {
+  return (
+    <article className="card">
+      <h2>{user.name}</h2>
+      <ul className="card__taglist">
+        {user.roles.map((role) => (
+          <Tag key={role} tag={role} />
+        ))}
+        {/* <Tag tag={user.roles[0]} /> */}
+      </ul>
+      <p>{user.about}</p>
+      <Button>edit</Button>
+      <Button buttonType="danger">invoice</Button>
+    </article>
+  );
+}
+
+export default Card;
---
+++ react-with-arrays_users/src/components/Tag.js
@@ -0,0 +1,6 @@
+import "./Tag.css";
+
+export default function Tag({ tag }) {
+  const isAdmin = tag === "admin";
+  return <li className={`tag ${isAdmin ? "tag--highlight" : "tag"}`}>{tag}</li>;
+}
