23f8032c5df3648421e3020bb9cd5bbc38aa8417
Jana Scheidemann <jana.scheidemann@freenet.de>
2024-03-18 17:07:18 +0100

    add edit and delete

--- backend-create_products/components/Product/index.js
+++ backend-create_products/components/Product/index.js
@@ -3,12 +3,45 @@ import { useRouter } from "next/router";
 import { ProductCard } from "./Product.styled";
 import Comments from "../Comments";
 import { StyledLink } from "../Link/Link.styled";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
+
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productData),
+    });
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct(id) {
+    const response = await fetch(`api/products/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      router.push("/");
+    } else {
+      console.log("error: ", response.status);
+    }
+  }
 
   if (isLoading) {
     return <h1>Loading...</h1>;
@@ -19,14 +52,28 @@ export default function Product() {
   }
 
   return (
-    <ProductCard>
-      <h2>{data.name}</h2>
-      <p>Description: {data.description}</p>
-      <p>
-        Price: {data.price} {data.currency}
-      </p>
-      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
-      <StyledLink href="/">Back to all</StyledLink>
-    </ProductCard>
+    <>
+      <ProductCard>
+        <h2>{data.name}</h2>
+        <p>Description: {data.description}</p>
+        <p>
+          Price: {data.price} {data.currency}
+        </p>
+        {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+        <StyledLink href="/">Back to all</StyledLink>
+      </ProductCard>
+      <button
+        type="button"
+        onClick={() => {
+          setIsEditMode(!isEditMode);
+        }}
+      >
+        edit
+      </button>
+      <button type="button" onClick={() => handleDeleteProduct(id)}>
+        delete
+      </button>
+      {isEditMode && <ProductForm onSubmit={handleEditProduct} />}
+    </>
   );
 }
